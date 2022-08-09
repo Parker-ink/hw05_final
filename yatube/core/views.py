f6f0d033fa8bc60fa8362f021dec7e8f34c19cca
Вадим Ковалев <minenikolasspace@bk.ru>
2022-08-09 23:47:33 +0300

    My first try to final

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from http import HTTPStatus
 
+
 def page_not_found(request, exception):
     # Переменная exception содержит отладочную информацию;
     # выводить её в шаблон пользовательской страницы 404 мы не станем
@@ -10,8 +11,10 @@ def page_not_found(request, exception):
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
 
+
 def permission_denied(request, exception):
     return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
+
 
 def server_lost(request):
     return render(request, "misc/500.html", status=500)
